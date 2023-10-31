23c42e2cd4f578e7de8893163791d64c7db7a547
coloradodev <fcolorado@deboraarango.edu.co>
2023-10-31 13:15:26 -0500

    Decoracion

--- escritura_rapida.py
+++ escritura_rapida.py
@@ -1,3 +1,4 @@
+# Los modulos que vamos a utilizar 
 import tkinter as tk
 import random
 import time
@@ -10,52 +11,60 @@ def fin():
  if frase.get() == frase_escrita.get() and frase.get():
   tiempo2 = time.time() 
   tiempo_final = str(tiempo2 - tiempo1.get())
+  frase_escrita.set("")
 
   resultado = "Has tardado "+tiempo_final+" segundos"
   etiqueta2 = tk.Label(root,text=resultado)
-  etiqueta2.place(x=130,y=230)
-  etiqueta2.config(bg="gray63",font=("Normal 12"))
+  etiqueta2.place(x=130,y=300)
+  etiqueta2.config(bg="gray5",font=("Normal 12"),fg="white")
 
-
+# La ventana principal de la interfaz grafica
 root = tk.Tk()
 root.title("Prueba de escritura rapida")
 root.geometry("600x400")
-root.config(bd=20,bg="gray63")
+root.config(bd=20,bg="gray5")
 root.resizable(0,0)
 
+# Variables para almacenar los datos que usaremos en
+# las diversas funciones del programa
 frase = tk.StringVar()
 frase_escrita = tk.StringVar()
 tiempo1 = tk.DoubleVar()
 
-
+# Lista de frases que se usaran para la prueba
 frases = ["Que bellos arboles hay aqui","Que bien nada ese pez",
           "El gato se asusta facilmente"]
 
 
+etiqueta1 = tk.Label(root,text="¡Bienvenido a la prueba de escritura rapida!")
+etiqueta1.place(x=120,y=10)
+etiqueta1.config(bg="gray5",font=("Normal 12"),fg="white")
 
-etiqueta1 = tk.Label(root,text="Escribe la frase de arriba:")
-etiqueta1.place(x=190,y=80)
-etiqueta1.config(bg="gray63",font=("Normal 12"))
+etiqueta2 = tk.Label(root,
+                      text="Pulsa el boton 'Iniciar' e intenta escribir la frase que aparezca \
+                        \ntan rapido como puedas, cuando termines pulsa 'Terminar'.",
+                      justify="left")
+etiqueta2.place(x=80,y=30)
+etiqueta2.config(bg="gray5",font=("Normal 12"),fg="white")
+
+etiqueta3 = tk.Label(root,text="Escribe la frase de arriba:")
+etiqueta3.place(x=190,y=150)
+etiqueta3.config(bg="gray5",font=("Normal 12"),fg="white")
 
 entrada1 = tk.Entry(root,bd=5,font="Normal 12",textvariable=frase,
-                    width=22,state="readonly")
-entrada1.place(x=170,y=20)
+                    width=22,bg="gray5",fg="white")
+entrada1.place(x=170,y=100)
 
-entrada2 = tk.Entry(root,bd=5,font="Normal 12",textvariable=frase_escrita,width=22)
-entrada2.place(x=170,y=120)
+entrada2 = tk.Entry(root,bd=5,font="Normal 12",textvariable=frase_escrita,
+                    width=22,bg="gray5",fg="white")
+entrada2.place(x=170,y=190)
 
-iniciar = tk.Button(root,text="Iniciar",font="Normal 10",bd=10,bg="gray63",
-                   width=9,command=inicio)
-iniciar.place(x=170,y=160)
+iniciar = tk.Button(root,text="Iniciar",font="Normal 10",bd=10,bg="gray5",
+                   width=9,command=inicio,fg="white")
+iniciar.place(x=170,y=240)
 
-terminar = tk.Button(root,text="Terminar",font="Normal 10",bd=10,bg="gray63",
-                   width=9,command=fin)
-terminar.place(x=280,y=160)
-
-
-
-
-
-
+terminar = tk.Button(root,text="Terminar",font="Normal 10",bd=10,bg="gray5",
+                   width=9,command=fin,fg="white")
+terminar.place(x=280,y=240)
 
 root.mainloop()
